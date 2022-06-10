16c3d82d73baf47bce9ba842d5d31f58a63a8600
frsargua <frsargua@gmail.com>
2022-06-10 15:35:54 +0100

    Render functions for table of contents and sections

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log("hello from node JS");
+// Render sections based on the "when" output
+
+// This render function renders the description, usage and Contributing.
+const renderSection = (sectionTitle, text) => {
+  const sectionEl = `##${sectionTitle}
+    ${text}    
+    `;
+};
+
+// This renders the table of contents
+const renderTC = (arrayOfAnswers) => {
+  console.log("Running renderTC function");
+  const arrayOfLinks = [
+    `##Table of Contents`,
+    `- [Installation](#installation)`,
+    `- [Description](#description)`,
+    `- [License](#license)`,
+    `- [Contributing](#contributing)`,
+    `- [Tests](#tests)`,
+  ];
+  let stringEl = arrayOfLinks[0];
+  for (let i = 1; i < arrayOfLinks.length; i++) {
+    if (arrayOfAnswers[i - 1]) {
+      stringEl = stringEl + "\n" + arrayOfLinks[i];
+    }
+  }
+};
+
+renderTC([true, false, true, true, true]);
